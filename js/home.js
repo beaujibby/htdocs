357167a9744efd0a25522486e368690358ee3d1c
beaujibby <Zachary@computer.local>
2016-05-02 08:04:03 -0700

    fixed slider for menu

--- js/home.js
+++ js/home.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
     $('#menutoggle').click(function(){
         if(left)
         {
-        $('.menubar').stop().animate({left: "+=140px"},500);
+        $('.menubar').stop().animate({left: "0px"},500);
         left=!left;
         }
         else
         {
-        $('.menubar').stop().animate({left: "-=140px"},500);
+        $('.menubar').stop().animate({left: "-140px"},500);
         left=!left;
         }
     });
