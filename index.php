7026aaabd4c5a20f2a2a833c855d8b32fe22a750
astrumxyz <astrumxyz@gmail.com>
2016-05-23 11:07:59 -0700

    may 23 2nd

--- index.php
+++ index.php
@@ -48,6 +48,7 @@ echo '<button type="submit" id="login-button" name="login">Login</button>';
 echo '</form>';
 echo '<a href="/register" style="text-decoration: none;font-size:14px; color: white;z-index:3;position:relative">dont have an account? sign up today.</a>';
 echo '<div class="about">Z&S</div>';
+//echo '<a class="about"href="about"><div class="about">Z&S</div></a>';
     
 echo '</div>';
 echo '<ul class="bg-bubbles">';
